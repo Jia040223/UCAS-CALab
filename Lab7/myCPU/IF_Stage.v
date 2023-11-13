f3fe45fdaf69898ed6fd29792b5209d95335a0f5
Jia040223 <1142747996@qq.com>
2023-11-13 15:19:46 +0800

    merge

--- Lab7/myCPU/IF_Stage.v
+++ Lab7/myCPU/IF_Stage.v
@@ -60,7 +60,7 @@ module IF_Stage(
 
 //IF statge control signal
     assign preif_ready_go   = inst_sram_req & inst_sram_addr_ok;
-    assign to_if_valid      = preif_ready_go & if_allowin & ~if_flush;
+    assign to_if_valid      = preif_ready_go & if_allowin;
 
     assign if_ready_go      = (inst_sram_data_ok | if_inst_reg_valid) & ~inst_cancel;
     assign if_allowin       = ~if_valid | if_ready_go & id_allowin | if_flush;     
@@ -70,7 +70,7 @@ module IF_Stage(
         if(~resetn)
             if_valid <= 0; 
         else if(if_allowin)
-            if_valid <= to_if_valid;            // 锟斤拷reset锟斤拷锟斤拷锟斤拷锟斤拷�?锟斤拷时锟斤拷锟斤拷锟斤拷锟截才匡拷始取�?
+            if_valid <= to_if_valid;            
         else if(br_taken | br_taken_reg)
             if_valid <= 0;
     end
