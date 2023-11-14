aa70ccf197e0e149c6d7aaf4d0b2a15f9ef4a96b
721753934 <jy721753934@163.com>
2023-11-14 11:22:35 +0800

    add AXI interface

--- Lab7/myCPU/mycpu_top.v
+++ Lab7/myCPU/mycpu_top.v
@@ -1,26 +1,47 @@
 module mycpu_top(
-    input  wire        clk,
-    input  wire        resetn,
-    // inst sram interface
-    output wire        inst_sram_req,
-    output wire        inst_sram_wr,
-    output wire [ 1:0] inst_sram_size,
-    output wire [ 3:0] inst_sram_wstrb,
-    output wire [31:0] inst_sram_addr,
-    output wire [31:0] inst_sram_wdata,
-    input  wire        inst_sram_addr_ok,
-    input  wire        inst_sram_data_ok,
-    input  wire [31:0] inst_sram_rdata,
-    // data sram interface
-    output wire        data_sram_req,
-    output wire        data_sram_wr,
-    output wire [ 1:0] data_sram_size,
-    output wire [ 3:0] data_sram_wstrb,
-    output wire [31:0] data_sram_addr,
-    output wire [31:0] data_sram_wdata,
-    input  wire        data_sram_addr_ok,
-    input  wire        data_sram_data_ok,
-    input  wire [31:0] data_sram_rdata,
+    input  wire        aclk,
+    input  wire        aresetn,
+    // read request interface
+    output wire [ 3:0] arid   ,
+    output wire [31:0] araddr ,
+    output wire [ 7:0] arlen  ,
+    output wire [ 2:0] arsize ,
+    output wire [ 1:0] arburst,
+    output wire [ 1:0] arlock ,
+    output wire [ 3:0] arcache,
+    output wire [ 2:0] arprot ,
+    output wire        arvalid,
+    input  wire        arready,
+    // read response interface
+    input  wire [ 3:0] rid    ,
+    input  wire [31:0] rdata  ,
+    input  wire [ 1:0] rresp  ,
+    input  wire        rlast  ,
+    input  wire        rvalid ,
+    output wire        rready ,
+    // write request interface
+    output wire [ 3:0] awid   ,
+    output wire [31:0] awaddr ,
+    output wire [ 7:0] awlen  ,
+    output wire [ 2:0] awsize ,
+    output wire [ 1:0] awburst,
+    output wire [ 1:0] awlock ,
+    output wire [ 3:0] awcache,
+    output wire [ 2:0] awprot ,
+    output wire        awvalid,
+    input  wire        awready,
+    // write data interface
+    output wire [ 3:0] wid    ,
+    output wire [31:0] wdata  ,
+    output wire [ 3:0] wstrb  ,
+    output wire        wlast  ,
+    output wire        wvalid ,
+    input  wire        wready ,
+    // write response interface
+    input  wire [ 3:0] bid    ,
+    input  wire [ 1:0] bresp  ,
+    input  wire        bvalid ,
+    output wire        bready ,
     // trace debug interface
     output wire [31:0] debug_wb_pc,
     output wire [ 3:0] debug_wb_rf_we,
@@ -73,10 +94,67 @@ module mycpu_top(
     
     wire [`WB_TO_IF_CSR_DATA_WIDTH -1:0]  wb_to_if_csr_data;
 
+    wire        inst_sram_req;
+    wire        inst_sram_wr;
+    wire [ 1:0] inst_sram_size;
+    wire [ 3:0] inst_sram_wstrb;
+    wire [31:0] inst_sram_addr;
+    wire [31:0] inst_sram_wdata;
+    wire        inst_sram_addr_ok;
+    wire        inst_sram_data_ok;
+    wire [31:0] inst_sram_rdata;
+    
+    wire        data_sram_req;
+    wire        data_sram_wr;
+    wire [ 1:0] data_sram_size;
+    wire [ 3:0] data_sram_wstrb;
+    wire [31:0] data_sram_addr;
+    wire [31:0] data_sram_wdata;
+    wire        data_sram_addr_ok;
+    wire        data_sram_data_ok;
+    wire [31:0] data_sram_rdata;
+
+    assign arid    = (data_sram_req)? 4'b1 : 4'b0;
+    assign araddr  = (arid)? data_sram_addr : inst_sram_addr;
+    assign arlen   = 8'b0;
+    assign arsize  = (arid)? data_sram_size : inst_sram_size;
+    assign arburst = 2'b01;
+    assign arlock  = 2'b0;
+    assign arcache = 4'b0;
+    assign arprot  = 3'b0;
+    assign arvalid = inst_sram_req | data_sram_req;
+    
+    assign rready  =
+    
+    assign awid    = 4'b1;
+    assign awaddr  = (arid)? data_sram_addr : inst_sram_addr;
+    assign awlen   = 8'b0;
+    assign awsize  = (arid)? data_sram_size : inst_sram_size;
+    assign awburst = 2'b01;
+    assign awlock  = 2'b0;
+    assign awcache = 4'b0;
+    assign awprot  = 3'b0;
+    assign awvalid = inst_sram_wr | data_sram_wr;
+    
+    assign wid     = 1'b1;
+    assign wdata   = (arid)? data_sram_wdata : inst_sram_wdata;
+    assign wstrb   = (arid)? data_sram_wstrb : inst_sram_wstrb;
+    assign wlast   = 1'b1;
+    assign wvalid  = inst_sram_wr | data_sram_wr;
+    
+    assign bready  =
+
+    assign inst_sram_addr_ok =
+    assign inst_sram_data_ok =
+    assign inst_sram_rdata   = 
+    assign data_sram_addr_ok =
+    assign data_sram_data_ok =
+    assign data_sram_rdata   =
+
     IF_Stage my_IF_Stage
     (
-        .clk(clk),
-        .resetn(resetn),
+        .clk(aclk),
+        .resetn(aresetn),
 
         .inst_sram_req(inst_sram_req),
         .inst_sram_wr(inst_sram_wr),
@@ -103,8 +181,8 @@ module mycpu_top(
 
     ID_Stage my_ID_Stage
     (
-        .clk(clk),
-        .resetn(resetn),
+        .clk(aclk),
+        .resetn(aresetn),
 
         .id_allowin(id_allowin),
         .br_taken(br_taken),
@@ -129,8 +207,8 @@ module mycpu_top(
 
     EX_Stage my_EX_Stage
     (
-        .clk(clk),
-        .resetn(resetn),
+        .clk(aclk),
+        .resetn(aresetn),
         
         .ex_allowin(ex_allowin),
         .id_to_ex_valid(id_to_ex_valid),
@@ -158,8 +236,8 @@ module mycpu_top(
 
     MEM_Stage my_MEM_Stage
     (
-        .clk(clk),
-        .resetn(resetn),
+        .clk(aclk),
+        .resetn(aresetn),
 
         .mem_allowin(mem_allowin),
         .ex_to_mem_valid(ex_to_mem_valid),
@@ -183,8 +261,8 @@ module mycpu_top(
 
     WB_Stage my_WB_Stage
     (
-        .clk(clk),
-        .resetn(resetn),
+        .clk(aclk),
+        .resetn(aresetn),
 
         .wb_allowin(wb_allowin),
         .mem_to_wb_valid(mem_to_wb_valid),
