ac19de7ad2e8e929bd178557480ac980cc6caab1
721753934 <jy721753934@163.com>
2023-11-30 00:37:12 +0800

    add TLB

--- Lab8/myCPU/TLB.v
+++ Lab8/myCPU/TLB.v
@@ -4,6 +4,7 @@ module tlb
 )
 (
     input  wire        clk,
+    input  wire        reset,
 
     //search port 0 (for inst fetch)
     input  wire [18:0] s0_vppn,
@@ -75,9 +76,9 @@ module tlb
 
     reg  [TLBNUM-1:0] tlb_e;
     reg  [TLBNUM-1:0] tlb_ps4MB; //pagesize 1:4MB 0:4KB
-    reg  [18:0] tlb_vppn;
-    reg  [ 9:0] tlb_asid;
-    reg         tlb_g;
+    reg  [18:0] tlb_vppn [TLBNUM-1:0];
+    reg  [ 9:0] tlb_asid [TLBNUM-1:0];
+    reg         tlb_g    [TLBNUM-1:0];
 
     reg  [19:0] tlb_ppn0 [TLBNUM-1:0];
     reg  [ 1:0] tlb_plv0 [TLBNUM-1:0];
@@ -90,176 +91,125 @@ module tlb
     reg  [ 1:0] tlb_mat1 [TLBNUM-1:0];
     reg         tlb_d1   [TLBNUM-1:0];
     reg         tlb_v1   [TLBNUM-1:0];
-
+    
+    // search
     wire [TLBNUM-1:0] match0;
-
-    assign match0[ 0] = (s0_vppn[18:10] == tlb_vppn[ 0][18:10])
-                     && (tlb_ps4MB[ 0] || s0_vppn[9:0] == tlb_vppn[ 0][9:0])
-                     && ((s0_asid == tlb_asid[ 0]) || tlb_g[ 0]);
-    assign match0[ 1] = (s0_vppn[18:10] == tlb_vppn[ 1][18:10])
-                     && (tlb_ps4MB[ 1] || s0_vppn[9:0] == tlb_vppn[ 1][9:0])
-                     && ((s0_asid == tlb_asid[ 1]) || tlb_g[ 1]);
-    assign match0[ 2] = (s0_vppn[18:10] == tlb_vppn[ 2][18:10])
-                     && (tlb_ps4MB[ 2] || s0_vppn[9:0] == tlb_vppn[ 2][9:0])
-                     && ((s0_asid == tlb_asid[ 2]) || tlb_g[ 2]);
-    assign match0[ 3] = (s0_vppn[18:10] == tlb_vppn[ 3][18:10])
-                     && (tlb_ps4MB[ 3] || s0_vppn[9:0] == tlb_vppn[ 3][9:0])
-                     && ((s0_asid == tlb_asid[ 3]) || tlb_g[ 3]);
-    assign match0[ 4] = (s0_vppn[18:10] == tlb_vppn[ 4][18:10])
-                     && (tlb_ps4MB[ 4] || s0_vppn[9:0] == tlb_vppn[ 4][9:0])
-                     && ((s0_asid == tlb_asid[ 4]) || tlb_g[ 4]);
-    assign match0[ 5] = (s0_vppn[18:10] == tlb_vppn[ 5][18:10])
-                     && (tlb_ps4MB[ 5] || s0_vppn[9:0] == tlb_vppn[ 5][9:0])
-                     && ((s0_asid == tlb_asid[ 5]) || tlb_g[ 5]);
-    assign match0[ 6] = (s0_vppn[18:10] == tlb_vppn[ 6][18:10])
-                     && (tlb_ps4MB[ 6] || s0_vppn[9:0] == tlb_vppn[ 6][9:0])
-                     && ((s0_asid == tlb_asid[ 6]) || tlb_g[ 6]);
-    assign match0[ 7] = (s0_vppn[18:10] == tlb_vppn[ 7][18:10])
-                     && (tlb_ps4MB[ 7] || s0_vppn[9:0] == tlb_vppn[ 7][9:0])
-                     && ((s0_asid == tlb_asid[ 7]) || tlb_g[ 7]);
-    assign match0[ 8] = (s0_vppn[18:10] == tlb_vppn[ 8][18:10])
-                     && (tlb_ps4MB[ 8] || s0_vppn[9:0] == tlb_vppn[ 8][9:0])
-                     && ((s0_asid == tlb_asid[ 8]) || tlb_g[ 8]);
-    assign match0[ 9] = (s0_vppn[18:10] == tlb_vppn[ 9][18:10])
-                     && (tlb_ps4MB[ 9] || s0_vppn[9:0] == tlb_vppn[ 9][9:0])
-                     && ((s0_asid == tlb_asid[ 9]) || tlb_g[ 9]);
-    assign match0[10] = (s0_vppn[18:10] == tlb_vppn[10][18:10])
-                     && (tlb_ps4MB[10] || s0_vppn[9:0] == tlb_vppn[10][9:0])
-                     && ((s0_asid == tlb_asid[10]) || tlb_g[10]);
-    assign match0[11] = (s0_vppn[18:10] == tlb_vppn[11][18:10])
-                     && (tlb_ps4MB[11] || s0_vppn[9:0] == tlb_vppn[11][9:0])
-                     && ((s0_asid == tlb_asid[11]) || tlb_g[11]);
-    assign match0[12] = (s0_vppn[18:10] == tlb_vppn[12][18:10])
-                     && (tlb_ps4MB[12] || s0_vppn[9:0] == tlb_vppn[12][9:0])
-                     && ((s0_asid == tlb_asid[12]) || tlb_g[12]);
-    assign match0[13] = (s0_vppn[18:10] == tlb_vppn[13][18:10])
-                     && (tlb_ps4MB[13] || s0_vppn[9:0] == tlb_vppn[13][9:0])
-                     && ((s0_asid == tlb_asid[13]) || tlb_g[13]);
-    assign match0[14] = (s0_vppn[18:10] == tlb_vppn[14][18:10])
-                     && (tlb_ps4MB[14] || s0_vppn[9:0] == tlb_vppn[14][9:0])
-                     && ((s0_asid == tlb_asid[14]) || tlb_g[14]);
-    assign match0[15] = (s0_vppn[18:10] == tlb_vppn[15][18:10])
-                     && (tlb_ps4MB[15] || s0_vppn[9:0] == tlb_vppn[15][9:0])
-                     && ((s0_asid == tlb_asid[15]) || tlb_g[15]);
-
-    assign s0_found = (|match0);
-    assign s0_index = 
-    assign s0_ppn = (s0_found)?
-                    {20{match0[ 0]}} & tlb_ppn0[ 0] | {20{match0[ 1]}} & tlb_ppn0[ 1] | {20{match0[ 2]}} & tlb_ppn0[ 2] | {20{match0[ 3]}} & tlb_ppn0[ 3] |
-                    {20{match0[ 4]}} & tlb_ppn0[ 4] | {20{match0[ 5]}} & tlb_ppn0[ 5] | {20{match0[ 6]}} & tlb_ppn0[ 6] | {20{match0[ 7]}} & tlb_ppn0[ 7] |
-                    {20{match0[ 8]}} & tlb_ppn0[ 8] | {20{match0[ 9]}} & tlb_ppn0[ 9] | {20{match0[10]}} & tlb_ppn0[10] | {20{match0[11]}} & tlb_ppn0[11] |
-                    {20{match0[12]}} & tlb_ppn0[12] | {20{match0[13]}} & tlb_ppn0[13] | {20{match0[14]}} & tlb_ppn0[14] | {20{match0[15]}} & tlb_ppn0[15] 
-                    :
-    assign s0_ps =
-    assign s0_plv = (s0_found)?
-                    {2{match0[ 0]}} & tlb_plv0[ 0] | {2{match0[ 1]}} & tlb_plv0[ 1] | {2{match0[ 2]}} & tlb_plv0[ 2] | {2{match0[ 3]}} & tlb_plv0[ 3] |
-                    {2{match0[ 4]}} & tlb_plv0[ 4] | {2{match0[ 5]}} & tlb_plv0[ 5] | {2{match0[ 6]}} & tlb_plv0[ 6] | {2{match0[ 7]}} & tlb_plv0[ 7] |
-                    {2{match0[ 8]}} & tlb_plv0[ 8] | {2{match0[ 9]}} & tlb_plv0[ 9] | {2{match0[10]}} & tlb_plv0[10] | {2{match0[11]}} & tlb_plv0[11] |
-                    {2{match0[12]}} & tlb_plv0[12] | {2{match0[13]}} & tlb_plv0[13] | {2{match0[14]}} & tlb_plv0[14] | {2{match0[15]}} & tlb_plv0[15] 
-                    :
-    assign s0_mat = (s0_found)?
-                    {2{match0[ 0]}} & tlb_mat0[ 0] | {2{match0[ 1]}} & tlb_mat0[ 1] | {2{match0[ 2]}} & tlb_mat0[ 2] | {2{match0[ 3]}} & tlb_mat0[ 3] |
-                    {2{match0[ 4]}} & tlb_mat0[ 4] | {2{match0[ 5]}} & tlb_mat0[ 5] | {2{match0[ 6]}} & tlb_mat0[ 6] | {2{match0[ 7]}} & tlb_mat0[ 7] |
-                    {2{match0[ 8]}} & tlb_mat0[ 8] | {2{match0[ 9]}} & tlb_mat0[ 9] | {2{match0[10]}} & tlb_mat0[10] | {2{match0[11]}} & tlb_mat0[11] |
-                    {2{match0[12]}} & tlb_mat0[12] | {2{match0[13]}} & tlb_mat0[13] | {2{match0[14]}} & tlb_mat0[14] | {2{match0[15]}} & tlb_mat0[15] 
-                    :
-    assign s0_d = (s0_found)?
-                  match0[ 0] & tlb_d0[ 0] | match0[ 1] & tlb_d0[ 1] | match0[ 2] & tlb_d0[ 2] | match0[ 3] & tlb_d0[ 3] |
-                  match0[ 4] & tlb_d0[ 4] | match0[ 5] & tlb_d0[ 5] | match0[ 6] & tlb_d0[ 6] | match0[ 7] & tlb_d0[ 7] |
-                  match0[ 8] & tlb_d0[ 8] | match0[ 9] & tlb_d0[ 9] | match0[10] & tlb_d0[10] | match0[11] & tlb_d0[11] |
-                  match0[12] & tlb_d0[12] | match0[13] & tlb_d0[13] | match0[14] & tlb_d0[14] | match0[15] & tlb_d0[15] 
-                  :
-    assign s0_v = (s0_found)?
-                  match0[ 0] & tlb_v0[ 0] | match0[ 1] & tlb_v0[ 1] | match0[ 2] & tlb_v0[ 2] | match0[ 3] & tlb_v0[ 3] |
-                  match0[ 4] & tlb_v0[ 4] | match0[ 5] & tlb_v0[ 5] | match0[ 6] & tlb_v0[ 6] | match0[ 7] & tlb_v0[ 7] |
-                  match0[ 8] & tlb_v0[ 8] | match0[ 9] & tlb_v0[ 9] | match0[10] & tlb_v0[10] | match0[11] & tlb_v0[11] |
-                  match0[12] & tlb_v0[12] | match0[13] & tlb_v0[13] | match0[14] & tlb_v0[14] | match0[15] & tlb_v0[15] 
-                  :
-
-
     wire [TLBNUM-1:0] match1;
 
-    assign match1[ 0] = (s1_vppn[18:10] == tlb_vppn[ 0][18:10])
-                     && (tlb_ps4MB[ 0] || s1_vppn[9:0] == tlb_vppn[ 0][9:0])
-                     && ((s1_asid == tlb_asid[ 0]) || tlb_g[ 0]);
-    assign match1[ 1] = (s1_vppn[18:10] == tlb_vppn[ 1][18:10])
-                     && (tlb_ps4MB[ 1] || s1_vppn[9:0] == tlb_vppn[ 1][9:0])
-                     && ((s1_asid == tlb_asid[ 1]) || tlb_g[ 1]);
-    assign match1[ 2] = (s1_vppn[18:10] == tlb_vppn[ 2][18:10])
-                     && (tlb_ps4MB[ 2] || s1_vppn[9:0] == tlb_vppn[ 2][9:0])
-                     && ((s1_asid == tlb_asid[ 2]) || tlb_g[ 2]);
-    assign match1[ 3] = (s1_vppn[18:10] == tlb_vppn[ 3][18:10])
-                     && (tlb_ps4MB[ 3] || s1_vppn[9:0] == tlb_vppn[ 3][9:0])
-                     && ((s1_asid == tlb_asid[ 3]) || tlb_g[ 3]);
-    assign match1[ 4] = (s1_vppn[18:10] == tlb_vppn[ 4][18:10])
-                     && (tlb_ps4MB[ 4] || s1_vppn[9:0] == tlb_vppn[ 4][9:0])
-                     && ((s1_asid == tlb_asid[ 4]) || tlb_g[ 4]);
-    assign match1[ 5] = (s1_vppn[18:10] == tlb_vppn[ 5][18:10])
-                     && (tlb_ps4MB[ 5] || s1_vppn[9:0] == tlb_vppn[ 5][9:0])
-                     && ((s1_asid == tlb_asid[ 5]) || tlb_g[ 5]);
-    assign match1[ 6] = (s1_vppn[18:10] == tlb_vppn[ 6][18:10])
-                     && (tlb_ps4MB[ 6] || s1_vppn[9:0] == tlb_vppn[ 6][9:0])
-                     && ((s1_asid == tlb_asid[ 6]) || tlb_g[ 6]);
-    assign match1[ 7] = (s1_vppn[18:10] == tlb_vppn[ 7][18:10])
-                     && (tlb_ps4MB[ 7] || s1_vppn[9:0] == tlb_vppn[ 7][9:0])
-                     && ((s1_asid == tlb_asid[ 7]) || tlb_g[ 7]);
-    assign match1[ 8] = (s1_vppn[18:10] == tlb_vppn[ 8][18:10])
-                     && (tlb_ps4MB[ 8] || s1_vppn[9:0] == tlb_vppn[ 8][9:0])
-                     && ((s1_asid == tlb_asid[ 8]) || tlb_g[ 8]);
-    assign match1[ 9] = (s1_vppn[18:10] == tlb_vppn[ 9][18:10])
-                     && (tlb_ps4MB[ 9] || s1_vppn[9:0] == tlb_vppn[ 9][9:0])
-                     && ((s1_asid == tlb_asid[ 9]) || tlb_g[ 9]);
-    assign match1[10] = (s1_vppn[18:10] == tlb_vppn[10][18:10])
-                     && (tlb_ps4MB[10] || s1_vppn[9:0] == tlb_vppn[10][9:0])
-                     && ((s1_asid == tlb_asid[10]) || tlb_g[10]);
-    assign match1[11] = (s1_vppn[18:10] == tlb_vppn[11][18:10])
-                     && (tlb_ps4MB[11] || s1_vppn[9:0] == tlb_vppn[11][9:0])
-                     && ((s1_asid == tlb_asid[11]) || tlb_g[11]);
-    assign match1[12] = (s1_vppn[18:10] == tlb_vppn[12][18:10])
-                     && (tlb_ps4MB[12] || s1_vppn[9:0] == tlb_vppn[12][9:0])
-                     && ((s1_asid == tlb_asid[12]) || tlb_g[12]);
-    assign match1[13] = (s1_vppn[18:10] == tlb_vppn[13][18:10])
-                     && (tlb_ps4MB[13] || s1_vppn[9:0] == tlb_vppn[13][9:0])
-                     && ((s1_asid == tlb_asid[13]) || tlb_g[13]);
-    assign match1[14] = (s1_vppn[18:10] == tlb_vppn[14][18:10])
-                     && (tlb_ps4MB[14] || s1_vppn[9:0] == tlb_vppn[14][9:0])
-                     && ((s1_asid == tlb_asid[14]) || tlb_g[14]);
-    assign match1[15] = (s1_vppn[18:10] == tlb_vppn[15][18:10])
-                     && (tlb_ps4MB[15] || s1_vppn[9:0] == tlb_vppn[15][9:0])
-                     && ((s1_asid == tlb_asid[15]) || tlb_g[15]);
-    
+    genvar i;
+    generate
+        for(i=0; i<TLBNUM; i++) begin
+            assign match0[i] = (s0_vppn[18:10] == tlb_vppn[i][18:10])
+                            && (tlb_ps4MB[i] || s0_vppn[9:0] == tlb_vppn[i][9:0])
+                            && ((s0_asid == tlb_asid[i]) || tlb_g[i]);
+            assign match1[i] = (s1_vppn[18:10] == tlb_vppn[i][18:10])
+                            && (tlb_ps4MB[i] || s1_vppn[9:0] == tlb_vppn[i][9:0])
+                            && ((s1_asid == tlb_asid[i]) || tlb_g[i]);
+        end
+    endgenerate
+
+    wire s0_whichpage;
+    wire s1_whichpage;
+
+    assign s0_found = (|match0);
+    assign s0_index = {4{match0[ 0]}} & 4'd0  | {4{match0[ 1]}} & 4'd1  | {4{match0[ 2]}} & 4'd2  | {4{match0[ 3]}} & 4'd3 
+                    | {4{match0[ 4]}} & 4'd4  | {4{match0[ 5]}} & 4'd5  | {4{match0[ 6]}} & 4'd6  | {4{match0[ 7]}} & 4'd7 
+                    | {4{match0[ 8]}} & 4'd8  | {4{match0[ 9]}} & 4'd9  | {4{match0[10]}} & 4'd10 | {4{match0[11]}} & 4'd11 
+                    | {4{match0[12]}} & 4'd12 | {4{match0[13]}} & 4'd13 | {4{match0[14]}} & 4'd14 | {4{match0[15]}} & 4'd15;
+    assign s0_whichpage = (tlb_ps4MB[s0_index])? s0_vppn[9]: s0_va_bit12;
+    assign s0_ps        = (tlb_ps4MB[s0_index]) ? 6'd22 : 6'd12;
+    assign s0_ppn       = (s0_whichpage) ? tlb_ppn1[s0_index] : tlb_ppn0[s0_index];
+    assign s0_plv       = (s0_whichpage) ? tlb_plv1[s0_index] : tlb_plv0[s0_index];
+    assign s0_mat       = (s0_whichpage) ? tlb_mat1[s0_index] : tlb_mat0[s0_index];
+    assign s0_d         = (s0_whichpage) ? tlb_d1  [s0_index] : tlb_d0  [s0_index];
+    assign s0_v         = (s0_whichpage) ? tlb_v1  [s0_index] : tlb_v0  [s0_index];
+
     assign s1_found = (|match1);
-    assign s1_index = 
-    assign s1_ppn = (s1_found)?
-                    {20{match1[ 0]}} & tlb_ppn1[ 0] | {20{match1[ 1]}} & tlb_ppn1[ 1] | {20{match1[ 2]}} & tlb_ppn1[ 2] | {20{match1[ 3]}} & tlb_ppn1[ 3] |
-                    {20{match1[ 4]}} & tlb_ppn1[ 4] | {20{match1[ 5]}} & tlb_ppn1[ 5] | {20{match1[ 6]}} & tlb_ppn1[ 6] | {20{match1[ 7]}} & tlb_ppn1[ 7] |
-                    {20{match1[ 8]}} & tlb_ppn1[ 8] | {20{match1[ 9]}} & tlb_ppn1[ 9] | {20{match1[10]}} & tlb_ppn1[10] | {20{match1[11]}} & tlb_ppn1[11] |
-                    {20{match1[12]}} & tlb_ppn1[12] | {20{match1[13]}} & tlb_ppn1[13] | {20{match1[14]}} & tlb_ppn1[14] | {20{match1[15]}} & tlb_ppn1[15] 
-                    :
-    assign s1_ps =
-    assign s1_plv = (s1_found)?
-                    {2{match1[ 0]}} & tlb_plv1[ 0] | {2{match1[ 1]}} & tlb_plv1[ 1] | {2{match1[ 2]}} & tlb_plv1[ 2] | {2{match1[ 3]}} & tlb_plv1[ 3] |
-                    {2{match1[ 4]}} & tlb_plv1[ 4] | {2{match1[ 5]}} & tlb_plv1[ 5] | {2{match1[ 6]}} & tlb_plv1[ 6] | {2{match1[ 7]}} & tlb_plv1[ 7] |
-                    {2{match1[ 8]}} & tlb_plv1[ 8] | {2{match1[ 9]}} & tlb_plv1[ 9] | {2{match1[10]}} & tlb_plv1[10] | {2{match1[11]}} & tlb_plv1[11] |
-                    {2{match1[12]}} & tlb_plv1[12] | {2{match1[13]}} & tlb_plv1[13] | {2{match1[14]}} & tlb_plv1[14] | {2{match1[15]}} & tlb_plv1[15] 
-                    :
-    assign s1_mat = (s1_found)?
-                    {2{match1[ 0]}} & tlb_mat1[ 0] | {2{match1[ 1]}} & tlb_mat1[ 1] | {2{match1[ 2]}} & tlb_mat1[ 2] | {2{match1[ 3]}} & tlb_mat1[ 3] |
-                    {2{match1[ 4]}} & tlb_mat1[ 4] | {2{match1[ 5]}} & tlb_mat1[ 5] | {2{match1[ 6]}} & tlb_mat1[ 6] | {2{match1[ 7]}} & tlb_mat1[ 7] |
-                    {2{match1[ 8]}} & tlb_mat1[ 8] | {2{match1[ 9]}} & tlb_mat1[ 9] | {2{match1[10]}} & tlb_mat1[10] | {2{match1[11]}} & tlb_mat1[11] |
-                    {2{match1[12]}} & tlb_mat1[12] | {2{match1[13]}} & tlb_mat1[13] | {2{match1[14]}} & tlb_mat1[14] | {2{match1[15]}} & tlb_mat1[15] 
-                    :
-    assign s1_d = (s1_found)?
-                  match1[ 0] & tlb_d1[ 0] | match1[ 1] & tlb_d1[ 1] | match1[ 2] & tlb_d1[ 2] | match1[ 3] & tlb_d1[ 3] |
-                  match1[ 4] & tlb_d1[ 4] | match1[ 5] & tlb_d1[ 5] | match1[ 6] & tlb_d1[ 6] | match1[ 7] & tlb_d1[ 7] |
-                  match1[ 8] & tlb_d1[ 8] | match1[ 9] & tlb_d1[ 9] | match1[10] & tlb_d1[10] | match1[11] & tlb_d1[11] |
-                  match1[12] & tlb_d1[12] | match1[13] & tlb_d1[13] | match1[14] & tlb_d1[14] | match1[15] & tlb_d1[15] 
-                  :
-    assign s1_v = (s1_found)?
-                  match1[ 0] & tlb_v1[ 0] | match1[ 1] & tlb_v1[ 1] | match1[ 2] & tlb_v1[ 2] | match1[ 3] & tlb_v1[ 3] |
-                  match1[ 4] & tlb_v1[ 4] | match1[ 5] & tlb_v1[ 5] | match1[ 6] & tlb_v1[ 6] | match1[ 7] & tlb_v1[ 7] |
-                  match1[ 8] & tlb_v1[ 8] | match1[ 9] & tlb_v1[ 9] | match1[10] & tlb_v1[10] | match1[11] & tlb_v1[11] |
-                  match1[12] & tlb_v1[12] | match1[13] & tlb_v1[13] | match1[14] & tlb_v1[14] | match1[15] & tlb_v1[15] 
-                  :
+    assign s1_index = {4{match1[ 0]}} & 4'd0  | {4{match1[ 1]}} & 4'd1  | {4{match1[ 2]}} & 4'd2  | {4{match1[ 3]}} & 4'd3 
+                    | {4{match1[ 4]}} & 4'd4  | {4{match1[ 5]}} & 4'd5  | {4{match1[ 6]}} & 4'd6  | {4{match1[ 7]}} & 4'd7 
+                    | {4{match1[ 8]}} & 4'd8  | {4{match1[ 9]}} & 4'd9  | {4{match1[10]}} & 4'd10 | {4{match1[11]}} & 4'd11 
+                    | {4{match1[12]}} & 4'd12 | {4{match1[13]}} & 4'd13 | {4{match1[14]}} & 4'd14 | {4{match1[15]}} & 4'd15;
+    assign s1_whichpage = (tlb_ps4MB[s1_index])? s1_vppn[9]: s1_va_bit12;
+    assign s1_ps        = (tlb_ps4MB[s1_index]) ? 6'd22 : 6'd12;
+    assign s1_ppn       = (s1_whichpage) ? tlb_ppn1[s1_index] : tlb_ppn0[s1_index];
+    assign s1_plv       = (s1_whichpage) ? tlb_plv1[s1_index] : tlb_plv0[s1_index];
+    assign s1_mat       = (s1_whichpage) ? tlb_mat1[s1_index] : tlb_mat0[s1_index];
+    assign s1_d         = (s1_whichpage) ? tlb_d1  [s1_index] : tlb_d0  [s1_index];
+    assign s1_v         = (s1_whichpage) ? tlb_v1  [s1_index] : tlb_v0  [s1_index];
+
+    // invtlb
+    wire [TLBNUM-1:0] cond1;
+    wire [TLBNUM-1:0] cond2;
+    wire [TLBNUM-1:0] cond3;
+    wire [TLBNUM-1:0] cond4;
+
+    wire [TLBNUM-1:0] invtlb_mask [31:0];
+
+    generate
+        for(i=0; i<TLBNUM; i++) begin
+            assign cond1[i] = ~tlb_g[i];
+            assign cond2[i] = tlb_g[i];
+            assign cond3[i] = s1_asid == tlb_asid[i];
+            assign cond4[i] = (s1_vppn[18:10] == tlb_vppn[i][18:10])
+                           && (tlb_ps4MB[i] || s1_vppn[9:0] == tlb_vppn[i][9:0]);
+        end
+    endgenerate
+
+    assign invtlb_mask[0] = 16'hffff;  
+    assign invtlb_mask[1] = 16'hffff;
+    assign invtlb_mask[2] = cond2;
+    assign invtlb_mask[3] = cond1;
+    assign invtlb_mask[4] = cond1 & cond3;
+    assign invtlb_mask[5] = cond1 & cond3 & cond4;
+    assign invtlb_mask[6] = (cond1|cond3) & cond4;
+    generate
+        for (i = 7; i < 32; i = i + 1)
+            assign invtlb_mask[i] = 16'b0;
+    endgenerate
+
+
+    // read
+    assign r_e    = tlb_e    [r_index];
+    assign r_vppn = tlb_vppn [r_index];
+    assign r_ps   = tlb_ps4MB[r_index];
+    assign r_g    = tlb_g    [r_index];
+
+    assign r_ppn0 = tlb_ppn0 [r_index];
+    assign r_plv0 = tlb_plv0 [r_index];
+    assign r_mat0 = tlb_mat0 [r_index];
+    assign r_d0   = tlb_d0   [r_index];
+    assign r_v0   = tlb_v0   [r_index];
+
+    assign r_ppn1 = tlb_ppn1 [r_index];
+    assign r_plv1 = tlb_plv1 [r_index];
+    assign r_mat1 = tlb_mat1 [r_index];
+    assign r_d1   = tlb_d1   [r_index];
+    assign r_v1   = tlb_v1   [r_index];
+
+    // write
+    always @(posedge clk) begin
+        if(we) begin
+            tlb_e    [w_index] <= w_e;
+            tlb_ps4MB[w_index] <= (w_ps == 6'd22);
+            tlb_vppn [w_index] <= w_vppn;
+            tlb_asid [w_index] <= w_asid;
+            tlb_g    [w_index] <= w_g;
+
+            tlb_ppn0 [w_index] <= w_ppn0;
+            tlb_plv0 [w_index] <= w_plv0;
+            tlb_mat0 [w_index] <= w_mat0;
+            tlb_d0   [w_index] <= w_d0;
+            tlb_v0   [w_index] <= w_v0;
+
+            tlb_ppn1 [w_index] <= w_ppn1;
+            tlb_plv1 [w_index] <= w_plv1;
+            tlb_mat1 [w_index] <= w_mat1;
+            tlb_d1   [w_index] <= w_d1;
+            tlb_v1   [w_index] <= w_v1;
+        end
+        else if(invtlb_valid) begin
+            tlb_e <= ~invtlb_mask[invtlb_op] & tlb_e;
+        end
+    end
 
 endmodule
